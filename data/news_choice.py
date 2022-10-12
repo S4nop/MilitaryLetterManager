471431aadf8647473d5b0eabd2bb7d7d9b6096da
Sunghee Ryu <sunghee.ryu@voyagerx.com>
2022-10-12 22:15:44 +0900

    뉴스 기사 선택지를 관리할 NewsChoice 클래스 생성

---
+++ data/news_choice.py
@@ -0,0 +1,45 @@
+class NewsChoice:
+    __choice_status: int
+
+    @property
+    def choice_status(self):
+        return self.__choice_status
+
+    def __init__(self):
+        self.__choice_status = 0
+
+    def set_politic_flag(self):
+        self.__choice_status |= 1
+
+    def get_politic_flag(self):
+        return self.__choice_status & 1
+
+    def set_economy_flag(self):
+        self.__choice_status |= (1 << 1)
+
+    def get_economy_flag(self):
+        return self.__choice_status & (1 << 1)
+
+    def set_society_flag(self):
+        self.__choice_status |= (1 << 2)
+
+    def get_society_flag(self):
+        return self.__choice_status & (1 << 2)
+
+    def set_lifeculture_flag(self):
+        self.__choice_status |= (1 << 3)
+
+    def get_lifeculture_flag(self):
+        return self.__choice_status & (1 << 3)
+
+    def set_world_flag(self):
+        self.__choice_status |= (1 << 4)
+
+    def get_world_flag(self):
+        return self.__choice_status & (1 << 4)
+
+    def set_itscience_flag(self):
+        self.__choice_status |= (1 << 5)
+
+    def get_itscience_flag(self):
+        return self.__choice_status & (1 << 5)
