7cc620c738a2d1ccd681e55d956d4631bb04b930
Sunghee Ryu <sunghee.ryu@voyagerx.com>
2022-11-03 21:56:29 +0900

    프로그램에 사용할 폰트와 initalize helper 클래스 추가

---
+++ helper/font_initalize_helper.py
@@ -0,0 +1,12 @@
+from PyQt5.QtGui import QFontDatabase
+
+
+class FontInitializeHelper:
+    @staticmethod
+    def init_fonts():
+        font_db = QFontDatabase()
+        font_db.addApplicationFont("./res/fonts/a꾸러기.ttf")
+        font_db.addApplicationFont("./res/fonts/a몬스터.ttf")
+        font_db.addApplicationFont("./res/fonts/a어린왕자M.ttf")
+        font_db.addApplicationFont("./res/fonts/NotoSansKR-Regular.ttf")
+        font_db.addApplicationFont("./res/fonts/Roboto-Regular.ttf")
