b12a374ae0abd944157afcfff69313d50cd92d01
Sunghee Ryu <sunghee.ryu@voyagerx.com>
2022-11-03 21:20:08 +0900

    카드뷰 클래스 생성

---
+++ ui/widgets/card/card_view.py
@@ -0,0 +1,37 @@
+from abc import abstractmethod
+
+from PyQt5.QtCore import QSize
+from PyQt5.QtGui import QMouseEvent
+from PyQt5.QtWidgets import QWidget, QLabel
+
+
+class CardView(QWidget):
+    HEIGHT_SIZE = 284
+    WIDTH_SIZE = 201
+
+    def __init__(self, parent):
+        super(CardView, self).__init__(parent)
+        self.__init_background()
+        self._init_foreground()
+        self.show()
+
+    def __init_background(self):
+        self._background = QLabel("", self)
+        self._background.move(0, 0)
+        self._background.resize(self.WIDTH_SIZE, self.HEIGHT_SIZE)
+        self._background.setStyleSheet('background-image: url("./res/img/card_view_bg.png");')
+        self.setFixedSize(self.WIDTH_SIZE, self.HEIGHT_SIZE)
+
+    def sizeHint(self) -> QSize:
+        return QSize(self.WIDTH_SIZE, self.HEIGHT_SIZE)
+
+    def mousePressEvent(self, a0: QMouseEvent) -> None:
+        self.on_click()
+
+    @abstractmethod
+    def _init_foreground(self):
+        raise NotImplementedError("This method must be implemented")
+
+    @abstractmethod
+    def on_click(self):
+        raise NotImplementedError("This method must be implemented")
