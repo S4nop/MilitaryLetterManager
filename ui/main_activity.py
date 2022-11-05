7ede310b61d482054e555c6c8050e94765c2d75e
Sunghee Ryu <sunghee.ryu@voyagerx.com>
2022-11-05 23:47:00 +0900

    메인액티비티 레이아웃 구현

--- ui/main_activity.py
+++ ui/main_activity.py
@@ -1,11 +1,21 @@
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import QMainWindow, QDesktopWidget, QLabel, QListWidget, QListView, QListWidgetItem, \
+    QAbstractItemView
 
 from client.main_client import MainClient
 from manager.thecamp_session_manager import TheCampSessionManager
+from res import resources
+from ui.widgets.card.add_action_card import AddActionCard
+from ui.widgets.card.soldier_card import SoldierCard
 from viewmodel.soldier_list_viewmodel import SoldierListViewModel
 
 
 class MainActivity(QMainWindow):
+
+    WINDOW_WIDTH = 1200
+    WINDOW_HEIGHT = 700
+
     main_client: MainClient = None
     solder_list_viewmodel: SoldierListViewModel = None
 
@@ -14,11 +24,15 @@ class MainActivity(QMainWindow):
         self.__init_ui()
         self.__init_client()
         self.__init_viewmodel()
+        self.__update_list_items(None)
         self.show()
 
     def __init_ui(self):
         self.setWindowTitle('Main')
-        self.setGeometry(300, 300, 300, 200)
+        self.setGeometry(0, 0, self.WINDOW_WIDTH, self.WINDOW_HEIGHT)
+        self.__init_background()
+        self.__init_list_view()
+        self.__move_window_to_center()
 
     def __init_client(self):
         session = TheCampSessionManager.get_instance().get_session()
@@ -33,3 +47,56 @@ class MainActivity(QMainWindow):
 
     def __update_soldier_list(self):
         self.solder_list_viewmodel.solder_list = self.main_client.get_soldier_data()
+
+    def __move_window_to_center(self):
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+    def __init_list_view(self):
+        self.list_widget = QListWidget(self)
+        self.list_widget.resize(1100, 340)
+        self.list_widget.move(40, 40)
+        self.list_widget.setFlow(QListView.LeftToRight)
+        self.list_widget.setStyleSheet('background-color: {}; border: 0px'.format(resources.color_primary))
+        self.list_widget.horizontalScrollBar().setStyleSheet('height: 0px')
+        self.list_widget.setHorizontalScrollMode(QAbstractItemView.ScrollPerPixel)
+        self.list_widget.setSpacing(20)
+
+    def __update_list_items(self, soldier_list):
+        if soldier_list is None:
+            soldier_list = [
+                SoldierCard(self, '3', '우지은'),
+                SoldierCard(self, '13', '류성희'),
+                SoldierCard(self, '23', '신용준'),
+                SoldierCard(self, '33', '윤상원'),
+                SoldierCard(self, '43', '김민수'),
+            ]
+        soldier_list.append(AddActionCard(self))
+
+        self.list_widget.clear()
+        for soldier_item in soldier_list:
+            list_item = QListWidgetItem(self.list_widget)
+            self.list_widget.addItem(list_item)
+            self.list_widget.setItemWidget(list_item, soldier_item)
+            list_item.setSizeHint(soldier_item.sizeHint())
+
+    def __init_background(self):
+        self.bg_top = QLabel(self)
+        self.bg_top.setAlignment(Qt.AlignHCenter)
+        self.bg_top.resize(self.WINDOW_WIDTH, 420)
+        self.bg_top.move(0, 0)
+        self.bg_top.setStyleSheet('background-color: {}'.format(resources.color_primary))
+
+        self.bg_bottom = QLabel(self)
+        self.bg_bottom.setAlignment(Qt.AlignHCenter)
+        self.bg_bottom.resize(self.WINDOW_WIDTH, 280)
+        self.bg_bottom.move(0, 420)
+        self.bg_bottom.setStyleSheet('background-color: {}'.format(resources.color_background))
+
+        self.title = QLabel('THE CAMP', self)
+        self.title.setFont(QFont('a몬스터', 56))
+        self.title.move(40, 500)
+        self.title.resize(self.WINDOW_WIDTH, 100)
+        self.title.setStyleSheet('Color: {}'.format(resources.color_primary))
