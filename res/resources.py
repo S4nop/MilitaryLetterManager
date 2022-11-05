87f53044ed1f8dbd336508d6415f43c695b01829
Sunghee Ryu <sunghee.ryu@voyagerx.com>
2022-11-05 23:16:02 +0900

    리소스를 관리할 resources.py 생성

---
+++ res/resources.py
@@ -0,0 +1,8 @@
+color_primary = '#00AFB0'
+color_background = '#FFFFFF'
+color_soldier_card_title = '#000000'
+color_soldier_card_text = '#565656'
+
+img_add = './res/img/add.png'
+img_card_view_bg = './res/img/card_view_bg.png'
+img_army_picture = './res/img/army_picture.png'
