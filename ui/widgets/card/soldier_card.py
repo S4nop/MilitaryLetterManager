6e7f60d0663549194a3af26132a9dd35366e95a8
Sunghee Ryu <sunghee.ryu@voyagerx.com>
2022-11-05 23:23:14 +0900

    기존에 생성된 레이아웃들이 사용중인 리소스들을 resources.py를 이용하도록 수정

--- ui/widgets/card/soldier_card.py
+++ ui/widgets/card/soldier_card.py
@@ -2,6 +2,7 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QFont, QPixmap
 from PyQt5.QtWidgets import QLabel
 
+from res import resources
 from ui.widgets.card.card_view import CardView
 
 
@@ -20,10 +21,11 @@ class SoldierCard(CardView):
         self.army_title.setFont(QFont('a몬스터', 36))
         self.army_title.move(0, 16)
         self.army_title.resize(self.WIDTH_SIZE, 100)
-        self.army_title.setStyleSheet('background-color: rgba(0, 0, 0, 0)')
+        self.army_title.setStyleSheet('background-color: rgba(0, 0, 0, 0); Color: {}'
+                                      .format(resources.color_soldier_card_title))
 
         self.army_picture = QLabel(self)
-        self.army_picture.setPixmap(QPixmap('./res/img/army_picture.png'))
+        self.army_picture.setPixmap(QPixmap(resources.img_army_picture))
         self.army_picture.setAlignment(Qt.AlignHCenter)
         self.army_picture.resize(self.WIDTH_SIZE, 120)
         self.army_picture.move(0, 80)
@@ -34,14 +36,16 @@ class SoldierCard(CardView):
         self.left_date_label.setFont(QFont('a어린왕자M', 32))
         self.left_date_label.move(0, 204)
         self.left_date_label.resize(self.WIDTH_SIZE, 100)
-        self.left_date_label.setStyleSheet('background-color: rgba(0, 0, 0, 0); Color: #565656')
+        self.left_date_label.setStyleSheet('background-color: rgba(0, 0, 0, 0); Color: {}'
+                                           .format(resources.color_soldier_card_text))
 
         self.soldier_name_label = QLabel("훈련병 " + self.soldier_name, self)
         self.soldier_name_label.setAlignment(Qt.AlignHCenter)
         self.soldier_name_label.setFont(QFont('a어린왕자M', 20))
         self.soldier_name_label.move(0, 244)
         self.soldier_name_label.resize(self.WIDTH_SIZE, 100)
-        self.soldier_name_label.setStyleSheet('background-color: rgba(0, 0, 0, 0); Color: #565656')
+        self.soldier_name_label.setStyleSheet('background-color: rgba(0, 0, 0, 0); Color: {}'
+                                              .format(resources.color_soldier_card_text))
 
     def on_click(self):
         pass
