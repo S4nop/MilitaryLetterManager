462bb921b3b02d9b6812b4465a580a8a7e7f1cb5
mikeysw <50179740+mikeysw@users.noreply.github.com>
2022-11-10 21:40:29 +0100

    Update login_activity.py
    제가 main_activity 하신거랑 최대한 비슷하게 만들었는데 res 가져와서 쓰는게 확실치 않아서 테스트 한번 해봐주시면 감사하겠습니다. 피드백 주시면 고치겠습니다!

--- ui/login_activity.py
+++ ui/login_activity.py
@@ -1,6 +1,14 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import Qt 
+from PyQt5.QtGui import QFont
+from PyQt5.QtGui import QFontDatabase
+
 from PyQt5 import QtGui
 from PyQt5.QtWidgets import QMainWindow, QPushButton
 
+from res import resources
 
 class LoginActivity(QMainWindow):
     def __init__(self):
@@ -12,9 +20,23 @@ class LoginActivity(QMainWindow):
         self.setWindowTitle('Login')
         self.setGeometry(300, 300, 300, 200)
 
+        self.title = QLabel("THE CAMP", self)
+        self.title.setFont(QFont('a몬스터', 20))
+        self.title.move(100, 0)
+        self.title.setStyleSheet('Color: {}'.format(resources.color_primary))
+
+        self.id_input = QLineEdit(" ", self)
+        self.id_input.move(100, 50)
+
+        self.pw_input = QLineEdit(" ", self)
+        self.pw_input.move(100, 100)
+
         self.login_button = QPushButton('Login', self)
-        self.login_button.move(100, 100)
+        self.login_button.move(100, 150)
+        self.login_button.setStyleSheet('Color: {}'.format(resources.color_primary))
         self.login_button.clicked.connect(self.login)
 
     def login(self):
+        ID = self.id_input.text()
+        PW = self.pw_input.text()
         self.close()
