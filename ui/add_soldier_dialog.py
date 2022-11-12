3e05ad8fbd74b64f8c8d39310134516400b490f7
Sunghee Ryu <sunghee.ryu@voyagerx.com>
2022-11-12 16:25:19 +0900

    ysw-ui-1 브랜치에서 작업한 내역 기반으로 add_soldier_dialog 레이아웃 교정

--- ui/add_soldier_dialog.py
+++ ui/add_soldier_dialog.py
@@ -9,8 +9,15 @@ from PyQt5 import QtGui
 from PyQt5.QtWidgets import QMainWindow, QPushButton
 
 from res import resources
+from ui.widgets.styled_button import StyledButton
+from ui.widgets.styled_text_field import StyledLineEdit
+from utils.utils import move_window_to_center
+
 
 class AddSoldierDialog(QMainWindow):
+    DIALOG_WIDTH = 400
+    DIALOG_HEIGHT = 250
+
     def __init__(self):
         super().__init__()
         self.__init_ui()
@@ -18,72 +25,60 @@ class AddSoldierDialog(QMainWindow):
 
     def __init_ui(self):
         self.setWindowTitle('Add Soldier')
-        self.setGeometry(300, 300, 600, 300)
+        self.setWindowFlag(Qt.FramelessWindowHint)
+        self.setGeometry(0, 0, self.DIALOG_WIDTH, self.DIALOG_HEIGHT)
+        move_window_to_center(self)
         self.__init_background()
         self.__init_elements()
 
     def __init_background(self):
         self.bg_left = QLabel(self)
         self.bg_left.setAlignment(Qt.AlignHCenter)
-        self.bg_left.resize(300, 300)
+        self.bg_left.resize(self.DIALOG_WIDTH / 2, self.DIALOG_HEIGHT)
         self.bg_left.move(0, 0)
-        self.bg_left.setStyleSheet('background-color: {}'.format(resources.color_background))
-        #self.bg_left.setStyleSheet('background-color: white')
+        self.bg_left.setStyleSheet('background-color: {}'.format(resources.color_white))
 
         self.bg_right = QLabel(self)
         self.bg_right.setAlignment(Qt.AlignHCenter)
-        self.bg_right.resize(300, 300)
-        self.bg_right.move(300, 0)
-        self.bg_right.setStyleSheet('background-color: {}'.format(resources.color_primary))
-        #self.bg_right.setStyleSheet('background-color: grey')
+        self.bg_left.resize(self.DIALOG_WIDTH / 2, self.DIALOG_HEIGHT)
+        self.bg_right.move(self.DIALOG_WIDTH / 2, 0)
+        self.bg_right.setStyleSheet('background-color: {}'.format(resources.color_gray_bg))
 
     def __init_elements(self):
-        self.left_widget = QWidget(self)
-        self.left_widget.resize(300, 300)
-        self.left_widget.move(0, 0)
+        self.ArmyType = QLabel("육군", self)
+        self.ArmyType.setFont(QFont('a몬스터', 40))
+        self.ArmyType.resize(self.DIALOG_WIDTH / 2, 100)
+        self.ArmyType.move(0, 10)
+        self.ArmyType.setAlignment(Qt.AlignCenter)
 
-        self.right_widget = QWidget(self)
-        self.right_widget.resize(300, 300)
-        self.right_widget.move(300, 0)
-
-        self.ArmyType = QLabel("육군")
-        self.ArmyType.setFont(QFont('a몬스터', 56))
-        self.ArmyType.setStyleSheet('Color: {}'.format(resources.color_primary))
-        #self.ArmyType.setAlignment(Qt.AlignCenter)
-
-        self.SoldierImage = QLabel()
-        self.SoldierImage.setPixmap(QPixmap('{}'.format(resources.army_picture)))
-        #self.SoldierImage.setPixmap(QPixmap("army_picture.png"))
+        self.SoldierImage = QLabel(self)
+        self.SoldierImage.setPixmap(QPixmap(resources.img_army_picture))
+        self.SoldierImage.resize(self.DIALOG_WIDTH / 2, 160)
+        self.SoldierImage.move(0, 80)
         self.SoldierImage.setAlignment(Qt.AlignCenter)
 
-        self.NameInput = QLineEdit()
+        self.NameInput = StyledLineEdit(self)
+        self.NameInput.resize(180, 32)
+        self.NameInput.move(int(self.DIALOG_WIDTH / 2) + 10, 30)
         self.NameInput.setPlaceholderText("성명")
-        self.NameInput.setFont(QFont('a꾸러기'))
 
-        self.EnlistDayInput = QLineEdit()
+        self.EnlistDayInput = StyledLineEdit(self)
+        self.EnlistDayInput.resize(180, 32)
+        self.EnlistDayInput.move(int(self.DIALOG_WIDTH / 2) + 10, 80)
         self.EnlistDayInput.setPlaceholderText("입영일(YYYY-MM-DD)")
-        self.EnlistDayInput.setFont(QFont('a꾸러기'))
 
-        self.BDayInput = QLineEdit()
+        self.BDayInput = StyledLineEdit(self)
+        self.BDayInput.resize(180, 32)
+        self.BDayInput.move(int(self.DIALOG_WIDTH / 2) + 10, 130)
         self.BDayInput.setPlaceholderText("생일(YYYY-MM-DD)")
-        self.BDayInput.setFont(QFont('a꾸러기'))
 
-        self.add_btn = QPushButton("Add")
-        self.add_btn.setFont(QFont('a꾸러기'))
-        self.add_btn.setStyleSheet('Color: {}'.format(resources.color_primary))
-        self.add_btn.clicked.connect(self.add)
+        self.add_btn = StyledButton(self)
+        self.add_btn.setText('Add')
+        self.add_btn.resize(180, 40)
+        self.add_btn.move(int(self.DIALOG_WIDTH / 2) + 10, 190)
+        self.add_btn.set_font_size(12)
+        self.add_btn.set_on_click(self.add)
 
-        layout_left = QVBoxLayout(self.left_widget)
-        layout_left.addWidget(self.ArmyType)
-        layout_left.addWidget(self.SoldierImage)
-        self.setLayout(layout_left)
-
-        layout_right = QVBoxLayout(self.right_widget)
-        layout_right.addWidget(self.NameInput)
-        layout_right.addWidget(self.EnlistDayInput)
-        layout_right.addWidget(self.BDayInput)
-        layout_right.addWidget(self.add_btn)
-        self.setLayout(layout_right)
 
     def add(self):
         Name = self.NameInput.text()
