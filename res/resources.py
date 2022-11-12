12239b2c3769ad7b317daad88038bc3164c4f652
Sunghee Ryu <sunghee.ryu@voyagerx.com>
2022-11-12 15:04:48 +0900

    ysw-ui 브랜치에서 작성한 코드 기반으로 디자인 교정

--- res/resources.py
+++ res/resources.py
@@ -3,6 +3,8 @@ color_main_bg = '#007D7D'
 color_white = '#FFFFFF'
 color_soldier_card_title = '#000000'
 color_soldier_card_text = '#565656'
+color_gray_bg = '#F3F5F6'
+color_text_field_border = '#C5C5C5'
 
 img_add = './res/img/add.png'
 img_card_view_bg = './res/img/card_view_bg.png'
--- ui/login_activity.py
+++ ui/login_activity.py
@@ -9,8 +9,14 @@ from PyQt5 import QtGui
 from PyQt5.QtWidgets import QMainWindow, QPushButton
 
 from res import resources
+from ui.widgets.styled_button import StyledButton
+from utils.utils import move_window_to_center
+
 
 class LoginActivity(QMainWindow):
+    WINDOW_WIDTH = 400
+    WINDOW_HEIGHT = 400
+
     def __init__(self):
         super().__init__()
         self.__init_ui()
@@ -18,23 +24,35 @@ class LoginActivity(QMainWindow):
 
     def __init_ui(self):
         self.setWindowTitle('Login')
-        self.setGeometry(300, 300, 300, 200)
+        self.setGeometry(0, 0, self.WINDOW_WIDTH, self.WINDOW_HEIGHT)
+        self.setWindowFlag(Qt.FramelessWindowHint)
+        self.setStyleSheet('background-color: {};'.format(resources.color_gray_bg))
+        move_window_to_center(self)
 
         self.title = QLabel("THE CAMP", self)
-        self.title.setFont(QFont('a몬스터', 20))
-        self.title.move(100, 0)
+        self.title.setFont(QFont('a몬스터', 40))
+        self.title.setAlignment(Qt.AlignCenter)
+        self.title.resize(self.WINDOW_WIDTH, 50)
+        self.title.move(0, 50)
         self.title.setStyleSheet('Color: {}'.format(resources.color_primary))
 
-        self.id_input = QLineEdit(" ", self)
-        self.id_input.move(100, 50)
+        self.id_input = QLineEdit("", self)
+        self.id_input.resize(300, 40)
+        self.id_input.move(50, 140)
+        self.id_input.setStyleSheet('background-color: {}; border-radius: 4px; border: 1px solid {}'
+                                    .format(resources.color_white, resources.color_text_field_border))
 
-        self.pw_input = QLineEdit(" ", self)
-        self.pw_input.move(100, 100)
+        self.pw_input = QLineEdit("", self)
+        self.pw_input.resize(300, 40)
+        self.pw_input.move(50, 190)
+        self.pw_input.setStyleSheet('background-color: {}; border-radius: 4px; border: 1px solid {}'
+                                    .format(resources.color_white, resources.color_text_field_border))
 
-        self.login_button = QPushButton('Login', self)
-        self.login_button.move(100, 150)
-        self.login_button.setStyleSheet('Color: {}'.format(resources.color_primary))
-        self.login_button.clicked.connect(self.login)
+        self.login_button = StyledButton(self)
+        self.login_button.set_text('Login')
+        self.login_button.set_on_click(self.login)
+        self.login_button.resize(300, 44)
+        self.login_button.move(50, 250)
 
     def login(self):
         ID = self.id_input.text()
