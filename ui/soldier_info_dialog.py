00c97badae6e7ce8720c01bfab2f685cad910bbb
Sunghee Ryu <sunghee.ryu@voyagerx.com>
2022-11-15 21:59:18 +0900

    SoldierInfoDialog에서 훈련병 정보를 받아 표시하도록 수정

--- ui/soldier_info_dialog.py
+++ ui/soldier_info_dialog.py
@@ -3,6 +3,7 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import Qt 
 from PyQt5.QtGui import QFont
 
+from data.soldier_letter_info import SoldierLetterInfo
 from res import resources
 from ui.widgets.shadow_rect_widget import ShadowRectFrame
 from ui.widgets.styled_button import StyledButton
@@ -13,11 +14,19 @@ class SoldierInfoDialog(QDialog):
     DIALOG_WIDTH = 400
     DIALOG_HEIGHT = 580
 
-    def __init__(self, parent):
+    name: str
+    left_day: str
+    letter_info: SoldierLetterInfo
+
+    def __init__(self, parent, name, left_day, letter_info, on_closed):
         super().__init__(parent, Qt.WindowFlags(Qt.FramelessWindowHint))
+        self.on_closed = on_closed
+        self.name = name
+        self.left_day = str(left_day)
+        self.letter_info = letter_info
         self.__init_ui()
         self.show()
-    
+
     def __init_ui(self):
         self.setGeometry(0, 0, self.DIALOG_WIDTH, self.DIALOG_HEIGHT)
         self.setStyleSheet('background-color: {};'.format(resources.color_gray_bg))
@@ -51,7 +60,7 @@ class SoldierInfoDialog(QDialog):
         self.soldier_image.move(120, 10)
         self.soldier_image.setAlignment(Qt.AlignCenter)
 
-        self.d_day = QLabel("D-3", self.bg_top)
+        self.d_day = QLabel("D-" + self.left_day, self.bg_top)
         self.d_day.setAlignment(Qt.AlignCenter)
         self.d_day.setStyleSheet('background-color: rgba(0, 0, 0, 0); Color: {};'
                                  .format(resources.color_login_activity_text))
@@ -59,7 +68,7 @@ class SoldierInfoDialog(QDialog):
         self.d_day.resize(100, 50)
         self.d_day.move(272, 42)
 
-        self.soldier_name = QLabel("훈련병 우지은", self.bg_top)
+        self.soldier_name = QLabel("훈련병 " + self.name, self.bg_top)
         self.soldier_name.setAlignment(Qt.AlignCenter)
         self.soldier_name.setStyleSheet('background-color: rgba(0, 0, 0, 0); Color: {};'
                                         .format(resources.color_login_activity_text))
@@ -79,7 +88,7 @@ class SoldierInfoDialog(QDialog):
         self.last_send_date_title.resize(200, 40)
         self.last_send_date_title.move(20, 20)
 
-        self.last_send_date = QLabel("2022-11-03", self.bg_mid)
+        self.last_send_date = QLabel(self.letter_info.last_sent_date, self.bg_mid)
         self.last_send_date.setAlignment(Qt.AlignRight)
         self.last_send_date.setStyleSheet('background-color: rgba(0, 0, 0, 0);')
         self.last_send_date.setFont(QFont('Roboto', 12))
@@ -93,7 +102,7 @@ class SoldierInfoDialog(QDialog):
         self.sent_letter_count_title.resize(200, 40)
         self.sent_letter_count_title.move(20, 60)
 
-        self.sent_letter_count = QLabel("15개", self.bg_mid)
+        self.sent_letter_count = QLabel(str(self.letter_info.letter_count), self.bg_mid)
         self.sent_letter_count.setAlignment(Qt.AlignRight)
         self.sent_letter_count.setStyleSheet('background-color: rgba(0, 0, 0, 0);')
         self.sent_letter_count.setFont(QFont('Roboto', 12))
@@ -101,6 +110,8 @@ class SoldierInfoDialog(QDialog):
         self.sent_letter_count.move(250, 60)
 
     def __init_third_frame(self):
+        letter_category_choice = self.letter_info.letter_news_category
+
         self.bg_bottom = ShadowRectFrame(self)
         self.bg_bottom.resize(390, 240)
         self.bg_bottom.move(5, 276)
@@ -114,16 +125,22 @@ class SoldierInfoDialog(QDialog):
 
         self.politics = QCheckBox("정치", self.bg_bottom)
         self.politics.setStyleSheet('background-color: rgba(0, 0, 0, 0); Color: #3d3d3d;')
+        self.politics.setChecked(letter_category_choice.get_politic_flag() == 1)
         self.economy = QCheckBox("경제", self.bg_bottom)
         self.economy.setStyleSheet('background-color: rgba(0, 0, 0, 0); Color: #3d3d3d;')
+        self.economy.setChecked(letter_category_choice.get_economy_flag() == 1)
         self.society = QCheckBox("사회", self.bg_bottom)
         self.society.setStyleSheet('background-color: rgba(0, 0, 0, 0); Color: #3d3d3d;')
+        self.society.setChecked(letter_category_choice.get_society_flag() == 1)
         self.culture = QCheckBox("생활/문화", self.bg_bottom)
         self.culture.setStyleSheet('background-color: rgba(0, 0, 0, 0); Color: #3d3d3d;')
+        self.culture.setChecked(letter_category_choice.get_lifeculture_flag() == 1)
         self.it_science = QCheckBox("IT/과학", self.bg_bottom)
         self.it_science.setStyleSheet('background-color: rgba(0, 0, 0, 0); Color: #3d3d3d;')
-        self.sports = QCheckBox("스포츠", self.bg_bottom)
-        self.sports.setStyleSheet('background-color: rgba(0, 0, 0, 0); Color: #3d3d3d;')
+        self.it_science.setChecked(letter_category_choice.get_itscience_flag() == 1)
+        self.worlds = QCheckBox("세계", self.bg_bottom)
+        self.worlds.setStyleSheet('background-color: rgba(0, 0, 0, 0); Color: #3d3d3d;')
+        self.worlds.setChecked(letter_category_choice.get_world_flag() == 1)
 
         layout_bottom = QGridLayout(self.bg_bottom)
         layout_bottom.addWidget(self.politics, 1, 0)
@@ -131,10 +148,11 @@ class SoldierInfoDialog(QDialog):
         layout_bottom.addWidget(self.society, 2, 0)
         layout_bottom.addWidget(self.culture, 2, 1)
         layout_bottom.addWidget(self.it_science, 3, 0)
-        layout_bottom.addWidget(self.sports, 3, 1)
+        layout_bottom.addWidget(self.worlds, 3, 1)
         layout_bottom.setAlignment(Qt.AlignTop)
         layout_bottom.setContentsMargins(20, 60, 20, 20)
         layout_bottom.setSpacing(12)
 
     def on_click_close(self):
+        self.on_closed()
         self.close()
